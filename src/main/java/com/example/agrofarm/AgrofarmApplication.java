03d003856efb62473304f8f534136f08ed0250de
adityasachan24@gmail.com <adityasachan24@gmail.com>
2025-03-02 21:11:43 +0530

    initial commit

---
+++ src/main/java/com/example/agrofarm/AgrofarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.agrofarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrofarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrofarmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/agrofarm/AgrofarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agrofarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrofarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
